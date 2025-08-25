d3515a42e403fabd4d59c7f65d69d0b4f976b055
raufurislam <raufurislam@gmail.com>
2025-08-26 02:25:27 +0600

    Navigation bar mobile menu option fix

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -89,17 +89,41 @@ export default function Navbar() {
             <PopoverContent align="start" className="w-36 p-1 md:hidden">
               <NavigationMenu className="max-w-none *:w-full">
                 <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
-                  {navigationLinks.map((link, index) => (
-                    <NavigationMenuItem key={index} className="w-full">
-                      <NavigationMenuLink asChild className="py-1.5">
-                        <Link to={link.href}>{link.label} </Link>
-                      </NavigationMenuLink>
-                    </NavigationMenuItem>
-                  ))}
+                  {/* {navigationLinks
+                    .filter(
+                      (link) =>
+                        link.role === "PUBLIC" || link.role === data?.data?.role
+                    )
+                    .map((link) => (
+                      <NavigationMenuItem
+                        key={`${link.href}-${link.role}`}
+                        className="w-full"
+                      >
+                        <NavigationMenuLink asChild className="py-1.5">
+                          <Link to={link.href}>{link.label} </Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    ))} */}
+                  {navigationLinks
+                    .filter(
+                      (link) =>
+                        link.role === "PUBLIC" || link.role === data?.data?.role
+                    )
+                    .map((link) => (
+                      <NavigationMenuItem
+                        key={`${link.href}-${link.role}`}
+                        className="w-full"
+                      >
+                        <NavigationMenuLink asChild className="py-1.5">
+                          <Link to={link.href}>{link.label} </Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    ))}
                 </NavigationMenuList>
               </NavigationMenu>
             </PopoverContent>
           </Popover>
+
           {/* Main nav */}
           <div className="flex items-center gap-6">
             <Link to="/" className="text-primary hover:text-primary/90">
--- src/pages/Rider/DriverRequest.tsx
+++ src/pages/Rider/DriverRequest.tsx
@@ -1,3 +1,4 @@
+import { TriangleAlert } from "lucide-react";
 import { useLocation } from "react-router";
 
 export default function DriverRequest() {
@@ -6,6 +7,18 @@ export default function DriverRequest() {
   console.log("Driver request", basicInfo);
   return (
     <div>
+      <div className="rounded-md border border-amber-500/50 px-4 py-3 text-amber-600">
+        <p className="text-sm">
+          <TriangleAlert
+            className="me-3 -mt-0.5 inline-flex opacity-60"
+            size={16}
+            aria-hidden="true"
+          />
+          You must complete the information of your to request for driver. After
+          verification your role is seted to Driver. If you not provide your
+          information you can serve our website to as User.
+        </p>
+      </div>
       <h1>This is DriverRequest component</h1>
     </div>
   );
